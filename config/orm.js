4598d42712a723c833dc4b1d746102dede429d84
Joanne Kim <joannehyelee@gmail.com>
2018-06-03 14:55:23 -0700

    Update orm.js

--- config/orm.js
+++ config/orm.js
@@ -7,21 +7,31 @@ const CONNECTION = require("./connection.js");
 // These methods will be used to retrieve & store data in database
 // ================================================================
 const ORM = {
-    selectAll: function (colToSearch, tableInput) {
+    selectAll: function (burgerCol, tableInput) {
         // SELECT burger_name FROM burgers
         const QUERYSTRING = "SELECT ?? FROM ??";
-        CONNECTION.query(QUERYSTRING, [coltoSearch, tableInput], function (err, result){
+        CONNECTION.query(QUERYSTRING, [burgerCol, tableInput], function (err, result){
             if (err) throw err;
             console.log(result);
         });
     },
 
-    insertOne: function () {
+    insertOne: function (tableInput, burgerCol, devouredCol, burgerVal, devouredVal) {
         // INSERT INTO burgers (burger_name, devoured) VALUES ("", FALSE);
+        const QUERYSTRING = "INSERT INTO ?? (??, ??) VALUES (??, FALSE);";
+        CONNECTION.query(QUERYSTRING, [tableInput, burgerCol, devouredCol, burgerVal, devouredVal], function (err, result){
+            if (err) throw err;
+            console.log(result);
+        });
     },
 
-    updateOne: function () {
+    updateOne: function (tableInput, devouredCol, devouredVal, burgerCol, burgerVal) {
         // UPDATE burgers SET devoured = true WHERE burger_name = "";
+        const QUERYSTRING = "UPDATE ?? SET ?? = ?? WHERE ?? = ??";
+        CONNECTION.query(QUERYSTRING, [tableInput, devouredCol, devouredVal, burgerCol, burgerVal], function(err, result){
+            if (err) throw err;
+            console.log(result);
+        });
     }
 };
 
